702e0b1b5c4fd3adeecffedfbfc29e7c87a62694
ziaprog <ziaprog@gmail.com>
2016-10-29 00:25:55 +0530

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WinProcessWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblChromeStatus = new System.Windows.Forms.Label();
+            this.lblStartTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblChromeStatus
+            // 
+            this.lblChromeStatus.AutoSize = true;
+            this.lblChromeStatus.Location = new System.Drawing.Point(51, 84);
+            this.lblChromeStatus.Name = "lblChromeStatus";
+            this.lblChromeStatus.Size = new System.Drawing.Size(61, 20);
+            this.lblChromeStatus.TabIndex = 0;
+            this.lblChromeStatus.Text = "[status]";
+            // 
+            // lblStartTime
+            // 
+            this.lblStartTime.AutoSize = true;
+            this.lblStartTime.Location = new System.Drawing.Point(51, 122);
+            this.lblStartTime.Name = "lblStartTime";
+            this.lblStartTime.Size = new System.Drawing.Size(161, 20);
+            this.lblStartTime.TabIndex = 1;
+            this.lblStartTime.Text = "[browser started time]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.BackgroundImage = global::WinProcessWatcher.Properties.Resources.swan_bird_lake_animal_37637_large__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(431, 250);
+            this.Controls.Add(this.lblStartTime);
+            this.Controls.Add(this.lblChromeStatus);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chrome Download Deleter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblChromeStatus;
+        private System.Windows.Forms.Label lblStartTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinProcessWatcher
+{
+    public partial class Form1 : Form
+    {
+        private DateTime startTime=DateTime.MinValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 10000;
+            timer1.Start();
+
+            //string[] files = Directory.GetFiles(dirName);
+
+            //foreach (string file in files)
+            //{
+            //    FileInfo fi = new FileInfo(file);
+            //    if (fi.LastAccessTime < DateTime.Now.AddMonths(-3))
+            //        fi.Delete();
+            //}
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName("chrome");
+            if (pname.Length == 0)
+            {
+                startTime = DateTime.MinValue;
+                lblChromeStatus.Text = "stopped" + DateTime.Now.ToString();
+                //delete files
+                (from f in new DirectoryInfo(@"C:\Users\junaidi\Downloads").GetFiles()
+                 where f.CreationTime > startTime
+                 select f).ToList()
+            .ForEach(f => f.Delete());
+                //close form
+                Application.Exit();
+            }
+            else
+            {
+                if (startTime == DateTime.MinValue)
+                {
+                    startTime = DateTime.Now;
+                    lblStartTime.Text = startTime.ToString();
+                }
+                lblChromeStatus.Text = "running" + DateTime.Now.ToString();
+            }
+        }
+    }
+}
